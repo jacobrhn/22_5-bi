f841775be2b900e1244588376962087b3cc27fb0
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2024-10-21 14:11:53 +0200

    complete

--- 24-10-10-vl/etl_prozess.R
+++ 24-10-10-vl/etl_prozess.R
@@ -1,7 +1,10 @@
+setwd("/Users/jacob/data/dhbw/22_5-bi/24-10-10-vl")
+#getwd()
+
 library(RSQLite)
 library(dplyr)
 library(Hmisc)
-
+library(lubridate)
 
 # Einlesen der Produktionskosten-Daten aus der CSV-Datei
 production_costs_data <- read.csv("productioncosts.csv", header = TRUE, sep= ",", dec=".")
@@ -15,45 +18,46 @@ con <- dbConnect(SQLite(), dbname = "sales_database.sqlite")
 # Extrahieren der Verkaufsdaten aus der Datenbanktabelle 'sales'
 sales_data <- dbReadTable(con, "sales")
 
-# Überprüfen der ersten Zeilen der Verkaufsdaten
+# ?berpr?fen der ersten Zeilen der Verkaufsdaten
 head(sales_data)
 
 
-# Schließen der Verbindung zur Datenbank
+# Schlie?en der Verbindung zur Datenbank
 dbDisconnect(con)
 
-# Zusammenführen der Produktionskosten- und Verkaufsdaten
+# Zusammenf?hren der Produktionskosten- und Verkaufsdaten
 merged_data <- production_costs_data %>%
   left_join(sales_data, by = c("Date", "Country"))
 
-# Überprüfen der ersten Zeilen der zusammengeführten Daten
+# ?berpr?fen der ersten Zeilen der zusammengef?hrten Daten
 head(merged_data)
 
-# Überprüfung auf fehlende Werte in jedem Feld des zusammengeführten Datensatzes
+# ?berpr?fung auf fehlende Werte in jedem Feld des zusammengef?hrten Datensatzes
 colSums(is.na(merged_data))
 
-
 #Verwendung der Hmisc-Packet und Ersetzen fehlender Produktionskosten mit dem Mittelwert:
+#merged_data$Production_Costs=impute(merged_data$Production_Costs, mean)
 merged_data$Production_Costs=impute(merged_data$Production_Costs, mean)
 
-# Filterung der Daten für das Jahr 2020
+# Filterung der Daten f?r das Jahr 2020
+merged_data$dates=ymd(merged_data$Date)
 filtered_data_2020 <- merged_data %>%
-  filter(Year == 2020)
+  filter(between(dates, as.Date("2020-01-01"), as.Date("2020-12-31")))
 
 # Anzeige der gefilterten Daten
 head(filtered_data_2020)
 
 
-# Filterung der Daten für Germany
+# Filterung der Daten f?r Germany
 filtered_data_germany <- merged_data %>%
   filter(Country == "Germany")
 
 
-# Filterung der Daten für Verkaufsumsatz größer als 1000
+# Filterung der Daten f?r Verkaufsumsatz gr??er als 1000
 high_revenue_data <- merged_data %>%
   filter(Sales_Revenue > 10000)
 
-# Kombinierte Filterung: Daten aus Deutschland und Verkaufsumsatz größer als 10000
+# Kombinierte Filterung: Daten aus Deutschland und Verkaufsumsatz gr??er als 10000
 filtered_combined <- merged_data %>%
   filter(Country == "Germany" & Sales_Revenue > 10000)
 
@@ -71,10 +75,10 @@ merged_data$Complaint_Rate <- merged_data$Opened_Complaints / merged_data$Active
 output <- "output.sqlite"
 con <- dbConnect(SQLite(), dbname = output)
 
-# Daten in die Datenbank schreiben (Überschreiben, falls die Tabelle bereits existiert)
+# Daten in die Datenbank schreiben (?berschreiben, falls die Tabelle bereits existiert)
 dbWriteTable(con, "merged_data", merged_data, overwrite = TRUE, row.names = FALSE)
 
-# Schließen der Verbindung zur Datenbank
+# Schlie?en der Verbindung zur Datenbank
 dbDisconnect(con)
 
 
